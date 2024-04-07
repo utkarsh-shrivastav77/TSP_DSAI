e4a3a0e6741ec410946885115d4db60ad2f0b435
Utkarsh Shrivastav <utkarsh.shrivastav202002@gmail.com>
2024-04-07 11:26:40 +0530

    Added the folder of week 1

---
+++ week_1/practice_problems/add_2.py
@@ -0,0 +1,7 @@
+def add_2(a,b):
+    return a+b
+
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : ")) 
+ans = add_2(num1,num2);
+print("The addition of two number is : " + str(ans))
